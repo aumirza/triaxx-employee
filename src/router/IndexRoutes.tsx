5577e9120ecc87459c2f7157fd64b179ccb54c32
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-11-19 01:53:38 +0530

    feat: refactor IndexRoutes to remove Zustand dependency and integrate Redux for user authentication

--- src/router/IndexRoutes.tsx
+++ src/router/IndexRoutes.tsx
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react';
-import { ProtectedRoutes } from './ProtectedRoutes';
-import { PublicRoutes } from './PublicRoutes';
-import { useSelector } from 'react-redux';
-import type { RootState } from '@/redux/store';
-import { useAuthStore } from '@/store/zustandStores';
+import React, { useEffect } from "react";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+import { PublicRoutes } from "./PublicRoutes";
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "@/redux/store";
+import { bootstrapAuthFromCookie } from "@/services/authHelpers";
+import { useGetUserByAuthQuery } from "@/redux/api/userApi";
+import { setCredentials } from "@/redux/slice/authSlice";
 
 export const IndexRoutes: React.FC = () => {
-  const { isAuthenticated } = useSelector((state: RootState) => state.auth);    
-  const {  checkAuth, fetchUserProfile } = useAuthStore();
+  const { isAuthenticated, token } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const dispatch = useDispatch();
+  // Bootstrap auth token from cookie if present
+  useEffect(() => {
+    bootstrapAuthFromCookie();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { data: userResp } = useGetUserByAuthQuery(undefined, { skip: !token });
 
   useEffect(() => {
-        checkAuth();
-    }, [checkAuth]);
-
-    useEffect(() => {
-        if (isAuthenticated) {
-            fetchUserProfile();
-        }
-    }, [isAuthenticated, fetchUserProfile]);
+    if (userResp && userResp?.data) {
+      dispatch(setCredentials({ token: token ?? "", user: userResp.data }));
+    }
+  }, [userResp, dispatch, token]);
 
   return isAuthenticated ? <ProtectedRoutes /> : <PublicRoutes />;
 };
